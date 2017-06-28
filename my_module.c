1dfd612dee30688e17dbfbf2c180ee1a37b76cb6
Garvit Khatri <garvitdelhi@gmail.com>
2017-06-28 15:42:42 +0530

    Adding files

--- my_module.c
+++ my_module.c
@@ -1,33 +1,29 @@
-/*  Example of wrapping cos function from math.h with the Python-C-API. */
-
 #include <Python.h>
 
-/*  wrapped cosine function */
 static PyObject* count_custom(PyObject* self, PyObject* args)
 {
-    long long int value;
+    long long int value, i;
 
-    /*  parse the input, from python float to c double */
+    /*  parse the input, from python integer to c long long int */
     if (!PyArg_ParseTuple(args, "L", &value))
         return NULL;
     /* if the above function returns -1, an appropriate Python exception will
      * have been set, and the function simply returns NULL
      */
 
-    /* call cos from libm */
     Py_BEGIN_ALLOW_THREADS
-    sleep(1);
-    for (long long int i = 0; i < value; ++i) {}
+//    sleep(1);
+    for (i = 0; i < value; ++i) {
+    }
     Py_END_ALLOW_THREADS
 
-    /*  construct the output from cos, from c double to python float */
     return Py_BuildValue("i", 1);
 }
 
 /*  define functions in module */
 static PyMethodDef myModule[] =
 {
-     {"count_custom", count_custom, METH_VARARGS, "evaluate the cosine"}
+     {"count_custom", count_custom, METH_VARARGS, "evaluate the count"}
 };
 
 /* module initialization */
--- my_python_test.py
+++ my_python_test.py
@@ -2,15 +2,15 @@ import my_module as mm
 import time as t
 from threading import Thread
 
+N = int(10e8)
 t1 = t.time();
-th1 = Thread(target=mm.count_custom, args=(int(10e8), ))
-th2 = Thread(target=mm.count_custom, args=(int(10e8), ))
+th1 = Thread(target=mm.count_custom, args=(N//2, ))
+th2 = Thread(target=mm.count_custom, args=(N//2, ))
 th1.start()
 th2.start()
 th1.join()
 th2.join()
 print 'Two threads: ', t.time() - t1
 t1 = t.time();
-mm.count_custom(int(10e8))
-mm.count_custom(int(10e8))
+mm.count_custom(N)
 print 'single thread: ', t.time() - t1
